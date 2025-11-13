99befc1b3da4cc9e556e561892c9a82f9b7b21ad
1104-Luo-JiaXing <jiaxingl@unr.edu>
2025-11-12 19:04:37 -0800

    Added New Health Bar and Display Health Value to the player

--- Assets/Player/Player scripts/PlayerHealthBar.cs
+++ Assets/Player/Player scripts/PlayerHealthBar.cs
@@ -1,23 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHealthBar : MonoBehaviour
 {
     public PlayerHealth playerHealth;
     public Image darkHealthBarFill;
     public Image currentHealthBarFill;
-
+    public TextMeshProUGUI displayCounter;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         darkHealthBarFill.fillAmount = 1f;
         currentHealthBarFill.fillAmount = (float)playerHealth.currentHealth / playerHealth.maxHealth;
+        displayCounter.text = playerHealth.currentHealth.ToString() + "/100";
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentHealthBarFill.fillAmount = playerHealth.currentHealth / playerHealth.maxHealth;
+        currentHealthBarFill.fillAmount = (float)playerHealth.currentHealth / playerHealth.maxHealth;
+        displayCounter.text = playerHealth.currentHealth.ToString() + "/100";
     }
 }
