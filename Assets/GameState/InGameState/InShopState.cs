08814a2e6bfea5ec120c98f4bc5cbfd0c196cbe8
1104-Luo-JiaXing <jiaxingl@unr.edu>
2025-11-22 22:30:05 -0800

    Added basic functionality to the shop screen

--- Assets/GameState/InGameState/InShopState.cs
+++ Assets/GameState/InGameState/InShopState.cs
@@ -3,13 +3,83 @@ using UnityEngine.UI;
 
 public class InShopState : GameState
 {
+    GameObject shopScreen;
     public override void EnterState(GameStateController Game)
     {
+        
+        shopScreen = Game.GetShopScreen();
+
         // Implementation for entering the in-shop state
+        Game.ShowPlayerUI(false);
+        // open shop UI
+        shopScreen.SetActive(true);
+        // pause time
+        Time.timeScale = 0;
+        // Default to damage upgrades panel
+        FilterButton(0); 
 
-        //open shop UI
-        //pause time
+        //Exit shop button
+        Button xButton = shopScreen.transform.Find("Shop_XButton").GetComponent<Button>();
 
+        xButton.onClick.AddListener(() =>
+        {
+            Game.SetState(new gameIdleState());
+        });
+    
+        //In shop upgrade filter buttons
+        Button damageButton = shopScreen.transform.Find("Filter/Damage_Button").GetComponent<Button>();
+        Button farmButton = shopScreen.transform.Find("Filter/Farm_Button").GetComponent<Button>();
+        Button healthButton = shopScreen.transform.Find("Filter/Health_Button").GetComponent<Button>();
+
+        damageButton.onClick.AddListener(() =>
+        {
+            //Filter to show only damage upgrades
+            FilterButton(0);
+            
+            //shopScreen.GetComponent<Shop>().FilterUpgrades("Damage");
+        });
+
+        farmButton.onClick.AddListener(() =>
+        {
+            //Filter to show only farm upgrades
+            FilterButton(1);
+            //shopScreen.GetComponent<Shop>().FilterUpgrades("Farm");
+        });
+
+        healthButton.onClick.AddListener(() =>
+        {
+            //Filter to show only health upgrades
+            FilterButton(2);
+            //shopScreen.GetComponent<Shop>().FilterUpgrades("Health");
+        });
+
+    }
+
+    private void FilterButton(int index)
+    {
+        
+        if(index == 0)
+        {
+            //Filter to show only damage upgrades
+            shopScreen.transform.Find("Filter/Damage_Items").gameObject.SetActive(true);
+            shopScreen.transform.Find("Filter/Farm_Items").gameObject.SetActive(false);
+            shopScreen.transform.Find("Filter/Health_Items").gameObject.SetActive(false);
+        }
+        else if(index == 1)
+        {
+            //Filter to show only farm upgrades
+            shopScreen.transform.Find("Filter/Damage_Items").gameObject.SetActive(false);
+            shopScreen.transform.Find("Filter/Farm_Items").gameObject.SetActive(true);
+            shopScreen.transform.Find("Filter/Health_Items").gameObject.SetActive(false);
+        }
+        else if(index == 2)
+        {
+            //Filter to show only health upgrades
+            shopScreen.transform.Find("Filter/Damage_Items").gameObject.SetActive(false);
+            shopScreen.transform.Find("Filter/Farm_Items").gameObject.SetActive(false);
+            shopScreen.transform.Find("Filter/Health_Items").gameObject.SetActive(true);
+        }
+        
     }
 
     public override void UpdateState(GameStateController Game)
@@ -19,8 +89,12 @@ public class InShopState : GameState
 
     public override void ExitState(GameStateController Game)
     {
-        //close shop UI
-        //resume time
+        // close shop UI
+        shopScreen.SetActive(false);
+        // resume time
+        Time.timeScale = 1;
         // Implementation for exiting the in-shop state
+        Game.ShowPlayerUI(true);
+
     }
 }
