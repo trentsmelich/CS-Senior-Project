52ba54ae600ea0f2df4a15ae1a62f0fa0421592a
Luis Garcia <shadonic3@gmail.com>
2025-12-05 00:08:30 -0800

    Made a level 2 and 3 farms

---
+++ Assets/GameState/MainMenu/Unlocks/StatModUnlock.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class StatModUnlock : UnlockParent
+{
+    private const string statModLvl1 = "unlock_statmod_lvl1";
+    private const string statModLvl2 = "unlock_statmod_lvl2";
+    private const string statModLvl3 = "unlock_statmod_lvl3";
+
+    public bool lvl1Unlocked;
+    public bool lvl2Unlocked;
+    public bool lvl3Unlocked;
+
+    public bool Lvl1Unlocked => lvl1Unlocked;
+    public bool Lvl2Unlocked => lvl2Unlocked;
+    public bool Lvl3Unlocked => lvl3Unlocked;
+
+    public override void LoadUnlockState()
+    {
+        lvl1Unlocked = PlayerPrefs.GetInt(statModLvl1, 0) == 1;
+        lvl2Unlocked = PlayerPrefs.GetInt(statModLvl2, 0) == 1;
+        lvl3Unlocked = PlayerPrefs.GetInt(statModLvl3, 0) == 1;
+    }
+
+    public override void Unlock(UnlockController UnlockController)
+    {
+        if (!lvl1Unlocked && UnlockController.GetNumTowers("StatModifier", 1) >= 10)
+        {
+            PlayerPrefs.SetInt(statModLvl1, 1);
+            lvl1Unlocked = true;
+        }
+
+        if (!lvl2Unlocked && UnlockController.GetNumTowers("StatModifier", 2) >= 10)
+        {
+            PlayerPrefs.SetInt(statModLvl2, 1);
+            lvl2Unlocked = true;
+        }
+
+        if (!lvl3Unlocked && UnlockController.GetNumTowers("StatModifier", 3) >= 10)
+        {
+            PlayerPrefs.SetInt(statModLvl3, 1);
+            lvl3Unlocked = true;
+        }
+
+        PlayerPrefs.Save();
+    }
+}
--- Assets/GameState/MainMenu/Unlocks/UnlockController.cs
+++ Assets/GameState/MainMenu/Unlocks/UnlockController.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class UnlockController : MonoBehaviour
 {
     [SerializeField] private UnlockParent[] unlocks;
-    //[SerializeField] private GameObject[] towers;
-
+    [SerializeField] private TowerParent[] towers;
      
     private static int numSlingshot1 = 0;
     private static int numSlingshot2 = 0;
@@ -12,26 +11,12 @@ public class UnlockController : MonoBehaviour
     private static int numCatapult1 = 0;
     private static int numCatapult2 = 0;
     private static int numCatapult3 = 0;
-
-    // void setUnlocks()
-    // {
-    //     //code to set unlocks based on player progress
-    //     // read the file, and set the unlocks accordingly
-    // }
-
-    // void updateUnlocks()
-    // {
-    //     //code to update unlocks when player achieves certain milestones
-    //     // loop on all unlocks and call their unlock method if conditions are met
-    //     // pass in the unlock controller to each unlock
-    //     // each individual unlock will check if it can be unlocked and then modify
-    //     // the save file
-
-    //     foreach (UnlockParent unlock in unlocks)
-    //     {
-    //         unlock.Unlock(this);
-    //     }
-    // }
+    private static int numFarm1 = 0;
+    private static int numFarm2 = 0;
+    private static int numFarm3 = 0;
+    private static int numStatMod1 = 0;
+    private static int numStatMod2 = 0;
+    private static int numStatMod3 = 0;
 
     private void Start()
     {
@@ -48,42 +33,76 @@ public class UnlockController : MonoBehaviour
 
     public void LoadSavedUnlocks()
     {
+        //pass in list of towers to each unlock to load their states
+        //inside each unlock, loop trhoug tower name is == to level I am in 
         foreach (UnlockParent unlock in unlocks)
         {
             unlock.LoadUnlockState();
         }
     }
 
+    
+
     public void IncreaseTowerCount (TowerParent towerParent, int level)
     {   
         if (towerParent is SlingShotTower)
         {
-            if(level == 1)
+            if (level == 1)
             {
                 numSlingshot1++;
             }
-            else if(level == 2)
+            else if (level == 2)
             {
                 numSlingshot2++;
             }
-            else if(level == 3)
+            else if (level == 3)
             {
                 numSlingshot3++;
             }
         }
-        else if(towerParent is Catapult)
+        else if (towerParent is Catapult)
         {
-            if(level == 1)
+            if (level == 1)
             {
                 numCatapult1++;
             }
-            else if(level == 2)
+            else if (level == 2)
             {
                 numCatapult2++;
             }
-            else if(level == 3)
+            else if (level == 3)
             {
                 numCatapult3++;
+            }
+        }
+        else if (towerParent is Farm)
+        {
+            if (level == 1)
+            {
+                numFarm1++;
+            }
+            else if (level == 2)
+            {
+                numFarm2++;
+            }
+            else if (level == 3)
+            {
+                numFarm3++;
+            }
+        }
+        else if (towerParent is StatModifier)
+        {
+            if (level == 1)
+            {
+                numStatMod1++;
+            }
+            else if (level == 2)
+            {
+                numStatMod2++;
+            }
+            else if (level == 3)
+            {
+                numStatMod3++;
             }
         }
     }
@@ -118,6 +137,36 @@ public class UnlockController : MonoBehaviour
             else if (level == 3)
             {
                 return numCatapult3;
+            }
+        }
+        else if (towerType == "Farm")
+        {
+            if (level == 1)
+            {
+                return numFarm1;
+            }
+            else if (level == 2)
+            {
+                return numFarm2;
+            }
+            else if (level == 3)
+            {
+                return numFarm3;
+            }
+        }
+        else if (towerType == "StatModifier")
+        {
+            if (level == 1)
+            {
+                return numStatMod1;
+            }
+            else if (level == 2)
+            {
+                return numStatMod2;
+            }
+            else if (level == 3)
+            {
+                return numStatMod3;
             }
         }
         return 0;
