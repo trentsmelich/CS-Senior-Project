74bf655f885bc99d451d24e062f087c83e76e485
Trent Parker Smelich <trentsmelich@unr.edu>
2025-12-08 02:59:10 -0800

    fixing farm prefab

--- Assets/Building Towers/Tower Scripts/TowerIdleState.cs
+++ Assets/Building Towers/Tower Scripts/TowerIdleState.cs
@@ -14,7 +14,6 @@ public class TowerIdleState : TowerState
     public override void UpdateState(TowerAI tower)
     {
         // Check for nearby enemies to switch to attack state
-        tower.GetComponent<SpriteRenderer>().color = Color.black;
         if(tower.GetTowerParent().TowerType == "Farm")
         {
             tower.SetState(new TowerAttackState());
@@ -24,7 +23,7 @@ public class TowerIdleState : TowerState
         if (enemiesInRange.Length > 0)
         {
             tower.targetEnemy = enemiesInRange[0].transform;
-            tower.GetComponent<SpriteRenderer>().color = Color.white;
+            
             tower.SetState(new TowerAttackState());
         }
         
