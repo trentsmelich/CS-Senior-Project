187073da0ae8e269245bcf892d081850481e65fc
Luis Garcia <shadonic3@gmail.com>
2026-02-19 14:41:24 -0800

    One special attack for one boss done, will do other attack later

--- Assets/Enemies/Individual Enemies/Bosses/WormBoss.cs
+++ Assets/Enemies/Individual Enemies/Bosses/WormBoss.cs
@@ -4,6 +4,15 @@ using UnityEngine.AI;
 
 public class WormBoss : EnemyParent
 {
+    [SerializeField] private Transform firePoint; // Point from which projectiles spawn
+    [SerializeField] private GameObject projectilePrefab; // Prefab of the projectile to shoot
+
+    [Header("Poison Attack")]
+    [SerializeField] private float specialAttackCooldown = 10f;
+    [SerializeField] private int specialPrjCount = 8;
+
+    private float angle;
+
     private bool specialActive = false;
     private Animator anim;
     private EnemyAI enemyAI;
@@ -14,6 +23,7 @@ public class WormBoss : EnemyParent
         anim = GetComponent<Animator>();
         enemyAI = GetComponent<EnemyAI>();
         agent = GetComponent<NavMeshAgent>();
+        angle = 360 / specialPrjCount;
 
         StartCoroutine(SpecialAttackController());
     }
@@ -64,8 +74,54 @@ public class WormBoss : EnemyParent
     {
         while (true)
         {
-            // whatever the logic is for the special attacks
-            // do later, no idea what I want the special attacks to be yet
+            yield return new WaitForSeconds(specialAttackCooldown);
+            StartCoroutine(PoisonProjectiles());
         }
     }
+
+    private IEnumerator AttackFromGround()
+    {
+        //figure out how to reverse an animation
+        //have animation of boss goes out of ground but not enter the ground
+        //attack is boss enters the ground, stays there for a bit, 
+        // then comes out of the ground on player position
+        // will do it 2 or 3 times in a row, immune while underground
+        yield return null;
+    }
+
+    private IEnumerator PoisonProjectiles()
+    {
+        //boss shoots projectiles in 8 directions
+        //if projectiles hits players, player gets poisoned and takes damage over time
+
+        if (!firePoint || projectilePrefab == null)
+        {
+            yield break;
+        }
+
+        specialActive = true;
+
+        FireProjectiles(0f, angle);
+
+        specialActive = false;
+    }
+
+    void FireProjectiles(float startAngle, float angleStep)
+    {
+        for (int i = 0; i < specialPrjCount; i++)
+        {
+            float prjAngle = startAngle + i * angleStep;
+            Vector2 dir = new Vector2(Mathf.Cos(prjAngle * Mathf.Deg2Rad), Mathf.Sin(prjAngle * Mathf.Deg2Rad)).normalized;
+
+            SpawnProjectile(dir);
+        }
+    }
+
+    void SpawnProjectile(Vector2 direction)
+    {
+        GameObject proj = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+        proj.GetComponent<PoisonProjectile>().SetProjectile(direction, enemyDamage);
+    }
+
+
 }
