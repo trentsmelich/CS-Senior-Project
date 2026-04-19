ec0c992033fb8bd8db99e23f3306451efc796507
1104-Luo-JiaXing <jiaxingl@unr.edu>
2026-04-19 15:27:37 -0700

    Added Health Bar to All Enemy Bosses in Prefab

---
+++ Assets/Enemies/EnemyHealthBar.cs
@@ -0,0 +1,35 @@
+
+// Libraries
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+//Author:Jia
+//Description: Displays the enemy's current health in health bar
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    [SerializeField] private Image healthBarFill;
+    [SerializeField] private GameObject enemy;
+    private EnemyHealth enemyHealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(enemy != null)
+        {
+            enemyHealth = enemy.GetComponent<EnemyHealth>();
+            healthBarFill.fillAmount = (float)enemyHealth.GetCurrentHealth() / enemyHealth.GetMaxHealth();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemy != null)
+        {
+            healthBarFill.fillAmount = (float)enemyHealth.GetCurrentHealth() / enemyHealth.GetMaxHealth();
+        }
+        
+    }
+}
